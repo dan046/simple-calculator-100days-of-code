ee5bb90104212216c415015e5422232fa57f3a23
dan046 <dandarellreyes@gmail.com>
2023-09-16 15:55:06 +0800

    revisited and refactored the todoList project

--- lessons/lesson12-advanced-todo-list/12-todo-list.js
+++ lessons/lesson12-advanced-todo-list/12-todo-list.js
@@ -1,59 +1,72 @@
-const inputField = document.getElementById("js-input-field")
-const addBtn = document.getElementById("js-add-btn")
-const displayItems = document.getElementById("js-list")
-const dateSelector = document.getElementById("js-date")
+const todo = document.getElementById("js-list")
+const todoForm = document.getElementById("js-todo-form")
+const formField = todoForm.elements["js-input-field"]
+const formDate = todoForm.elements["js-date"]
 
+// Create a dummy data
 let todoList = [
   {
+    name: "code, program",
+    date: "2023-09-08",
+  },
+  {
     name: "make dinner",
-    dueDate: "2023-09-07",
+    date: "2023-09-09",
   },
   {
     name: "wash dishes",
-    dueDate: "2023-09-08",
+    date: "2023-09-09",
   },
 ]
 
-function renderTodoList() {
+// Create a function to display the items
+const renderTodo = () => {
   let todoListHTML = ""
 
   todoList.forEach((items, index) => {
-    const { name, dueDate } = items
-
+    const { name, date } = items
     const html = `
-    <div>${name}</div>
-    <div>${dueDate}</div>
-    <button 
-    class="delete-btn"
-    onclick="
-    deleteTodo(${index}, ${1})
-    ">Delete</button>
+    <li class="list">
+    <p>${name}</p>
+    <p>${date}</p>
+    <button class="delete-btn" data-index="${index}">Delete</button>
+    </li>
     `
-    displayItems.innerHTML = todoListHTML += html
+    todoListHTML += html
+  })
+  todo.innerHTML = todoListHTML
+
+  // Add event listeners to delete buttons
+  const deleteButtons = document.querySelectorAll(".delete-btn")
+  // using querySelectorAll turns the delete button as an array. Meaning, we can use a loop for it.
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const index = e.target.getAttribute("data-index")
+      deleteTodo(index)
+    })
   })
 }
-renderTodoList()
 
-function deleteTodo(index, deleteCount) {
-  todoList.splice(index, deleteCount)
-  renderTodoList()
+const deleteTodo = (i) => {
+  todoList.splice(i, 1)
+  renderTodo()
 }
-// Array push method
-function addTodo() {
-  const name = inputField.value
-  const dueDate = dateSelector.value
 
-  if (!name || !dueDate) {
-    alert("fields must not be empty")
+// Add event listener for form submission
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const name = formField.value
+  const date = formDate.value
+  if (!name || !date) {
+    document.getElementById("js-error").innerHTML =
+      "<p class='error'>Fields must not be empty</p>"
     return
   }
+  todoList.push({ name, date })
+  renderTodo()
+  formField.value = ""
+  formDate.value = ""
+})
 
-  todoList.push({
-    name,
-    dueDate,
-  })
-
-  inputField.value = ""
-  dateSelector.value = ""
-  renderTodoList()
-}
+// Initial rendering of the todo list
+renderTodo()
